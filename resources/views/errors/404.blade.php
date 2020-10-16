4348249b6f705db40b3e1364ae03acd0af205b85
gigi603 <gilbert.trinidad1@gmail.com>
2020-10-16 21:34:29 +0200

    errors

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+@section('title', "Désolé la page demandée n'existe pas")
+@section('meta_description', "Vous demandez l'accès à une page qui ne figure pas sur notre application")
+@section('content')
+    <div class="container margin-top block-size" role="Page non trouvée sur atypikhouse">
+        <h1 class="h1-title">Page inexistante</h1>
+        <div class="row">
+            <div class="container">
+                <div class="block">
+                    Désolé mais la page que vous cherchez n'existe pas ou l'url correspondante n'est pas la bonne.
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+@section('footer', 'footer_absolute')
+@section('script')
+@endsection
---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+@section('title', "Désolé l")
+@section('meta_description', "Vous demandez l'accès à une page qui ne figure pas sur notre application")
+@section('content')
+    <div class="container margin-top block-size" role="Page non trouvée sur atypikhouse">
+        <h1 class="h1-title">Oups, une erreur est survenue</h1>
+        <div class="row">
+            <div class="container">
+                <div class="block">
+                    Désolé mais une erreur est survenue, nous travaillons et faisons tout notre possible pour régler ce problème dans les plus bref délais.
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+@section('footer', 'footer_absolute')
+@section('script')
+@endsection
