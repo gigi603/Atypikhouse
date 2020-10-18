f5206985738948e409c04b8e5f9bca74e377559e
gigi603 <gilbert.trinidad1@gmail.com>
2020-10-18 11:47:04 +0200

    redirection after authentificated

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -26,7 +26,7 @@ class RedirectIfAuthenticated
                 break;
             default:
                 if (Auth::guard($guard)->check()) {
-                    return redirect('/');
+                    return redirect(config('app.url'));
                 }
                 break;
         }
