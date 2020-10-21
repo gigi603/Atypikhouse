18a6cc884e2c20a7b4e626ed13a4672323430716
gigi603 <gilbert.trinidad1@gmail.com>
2020-10-21 11:38:22 +0200

    comment usercontroller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,28 +9,28 @@ use Illuminate\Notifications\Notifiable;
 
 class UserController extends Controller
 {
-    use Notifiable;
-    public function create(Request $request) {
-        $id = $request->input('name');
+    // use Notifiable;
+    // public function create(Request $request) {
+    //     $id = $request->input('name');
 
-        User::create([
-            'nom' => $request->input('nom'),
-            'prenom' => $request->input('prenom'),
-            'email' => $request->input('email'),
-            'password' => bcrypt($request->input('password'))
-        ]);
+    //     User::create([
+    //         'nom' => $request->input('nom'),
+    //         'prenom' => $request->input('prenom'),
+    //         'email' => $request->input('email'),
+    //         'password' => bcrypt($request->input('password'))
+    //     ]);
         
-        return response()->json('0');
-    }
+    //     return response()->json('0');
+    // }
 
-    public function get($id, Request $request) {
-        $user = User::find($id);
-        return response()->json($user);
-    }
+    // public function get($id, Request $request) {
+    //     $user = User::find($id);
+    //     return response()->json($user);
+    // }
 
-    public function delete($id, Request $request) {
-        $user = User::find($id);
-        $user->delete();
-        return response()->json('0');
-    }
+    // public function delete($id, Request $request) {
+    //     $user = User::find($id);
+    //     $user->delete();
+    //     return response()->json('0');
+    // }
 }
