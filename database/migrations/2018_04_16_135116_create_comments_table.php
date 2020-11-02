6509e0a42dc3d6e0da984a4dc2e3a447c16dc7eb
gigi603 <gilbert.trinidad1@gmail.com>
2020-11-02 16:56:08 +0100

    parent_id

--- database/migrations/2018_04_16_135116_create_comments_table.php
+++ database/migrations/2018_04_16_135116_create_comments_table.php
@@ -13,13 +13,14 @@ class CreateCommentsTable extends Migration
      */
     public function up()
     {
-        Schema::table('comments', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->increments('id');
             $table->string('comment');
             $table->integer('note'); 
             $table->integer('user_id');
             $table->integer('admin_id');
-            $table->integer('house_id');    
+            $table->integer('house_id');
+            $table->integer('parent_id')->unsigned()->nullable(false)->change();
             $table->timestamps();
         });
     }
