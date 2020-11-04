978d1d0828a5e603fb210a167d57396c9eb4dc44
gigi603 <gilbert.trinidad1@gmail.com>
2020-11-04 10:09:02 +0100

    testtt

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,47 +11,5 @@
                             <div class="row">
                                 <div class="col-lg-4 col-md-4 col-sm-12 cadre">
                                     <h1 class="title title-intro">Atypikhouse offre les meilleurs espaces atypiques en Europe !</h1>
-                                    <div class="form-group reservation-search">
-                                        @include('search',['url'=>'search','link'=>'search'])
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div id="block_home_2" role="avantages">
-        <div id="tranquilite" class="block_home_2_child">
-            <i class="fas fa-procedures fa-5x"></i>
-            <h2>Tranquilité</h2>
-            <p>Rester au calme pendant votre séjour dans nos habitats insolite. Nos cabanes et yourtes sauront combler vos désirs les plus variés</p>
-        </div>
-        <div id="depaysement" class="block_home_2_child">
-            <i class="fab fa-angellist fa-5x"></i>
-            <h2>Dépaysement</h2>
-            <p>Sortez de la routine quotidienne et venez vivre des expérience unique dans des décors à couper le souffle</p>
-        </div>
-        <div id="money" class="block_home_2_child">
-            <i class="far fa-money-bill-alt fa-5x"></i>
-            <h2>Economie</h2>
-            <p>Profitez de promotions toute l'année sur de nombreuses locations atypique tels que les cabanes, les cocons pour amoureux et bien d'autres. </p>
-        </div>
-    </div>
-    <div class="container-fluid background-houses" role="annonces">
-        <h2 class="hebergement-title">Parmis nos hebergements voici</h3>
-        <div class="row">
-            
-        </div>
-        {{-- <div class="text-right mb-3 mt-3">
-            <span>{{ $houses->links() }}</span>
-        </div> --}}
-    </div>
-    @section('script')
-        <script src="{{ asset('js/jquery.js') }}"></script>
-        <script src="{{ asset('js/jquery-ui.min.js') }}"></script>
-        <script src="{{ asset('js/calendarHome.js') }}"></script>
-    @endsection
+                                    
 @endsection
