148d9cdfff3f92fd953c44755b6beac0b4ec5c58
gigi603 <gilbert.trinidad1@gmail.com>
2020-11-06 16:58:46 +0100

    put app.js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -136,6 +136,7 @@
             </footer>
         </div>
         <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}"></script>
         <script src="{{ asset('js/jquery.min.js') }}"></script>
         <script src="{{ asset('js/jquery-ui.min.js') }}"></script>
         <script src="{{ asset('js/jquery.unveil.js') }}"></script>
