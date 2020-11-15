64edc14981448505ede01502478e092631c0f00d
gigi603 <gilbert.trinidad1@gmail.com>
2020-11-15 20:09:40 +0100

    fix bug propriete in create house step 3

--- public/js/create_house.js
+++ public/js/create_house.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+    $("input[name='propriete[]']").each(function () {
+        localStorage.setItem(this.id, $(this).prop('checked'));
+    })
     $("#select_category option:checked").each(function(){
         var house_id = $("#house_id").val();
         var category_id = $("#select_category option:checked").val();
@@ -8,11 +11,7 @@ $(document).ready(function(){
             url: site+'/json_propriete/'+house_id+'/'+category_id,
             dataType: "json",
             data: "",
-            success: function(data) {
-                // var idArr = [];
-                // for (j in data.valArray){
-                //     idArr.push(data.valArray[j].propriete_id);
-                // }                
+            success: function(data) {            
                 for (i in data.proprietes) {
                     $( ".proprietes" ).append(`
                     <div class="form-group">
@@ -25,13 +24,6 @@ $(document).ready(function(){
                     </div>`);
 
                     $("input[name='propriete[]']").each(function () {
-                        // if( $(this).is(':checked') ){
-                        //     $(this).val("true");
-                        //     console.log('true');
-                        // } else {
-                        //     $(this).val("false");
-                        //     console.log('false');
-                        // }
                         console.log('this', $(this));
                         $(this).prop('checked', localStorage.getItem(this.id) === 'true');
                     });      
@@ -51,24 +43,18 @@ $(document).ready(function(){
                 dataType: "json",
                 data: "",
                 success: function(data) {
-                    $('.proprietes').empty();
-                var idArr = [];
-
-                for (j in data.valArray){
-                    idArr.push(data.valArray[j].propriete_id);
-                }                
-
-                for (i in data.proprietes) {
-                    $( ".proprietes" ).append(`
-                    <div class="form-group">
-                        <label class="col-md-4 control-label">
-                            ${data.proprietes[i].propriete}
-                        </label>
-                        <div class="col-md-6">
-                            <input type="checkbox" class="checkbox_propertie" name="propriete[]" autofocus value="${data.proprietes[i].id}"/>
-                        </div>
-                    </div>`);
-                }
+                    $('.proprietes').empty();           
+                    for (i in data.proprietes) {
+                        $( ".proprietes" ).append(`
+                        <div class="form-group">
+                            <label class="col-md-4 control-label">
+                                ${data.proprietes[i].propriete}
+                            </label>
+                            <div class="col-md-6">
+                                <input type="checkbox" class="checkbox_propertie" name="propriete[]" autofocus value="${data.proprietes[i].id}"/>
+                            </div>
+                        </div>`);
+                    }
                 },error: function (data){
                     $('.proprietes').empty();
                 }
@@ -82,4 +68,3 @@ $(document).on("click", "input[name='propriete[]']", function(){
         localStorage.setItem(this.id, $(this).prop('checked'));
     })
 });
-/*<input type="hidden" name="propriete_id[]" value="${data.proprietes[i].id}"/>*/
