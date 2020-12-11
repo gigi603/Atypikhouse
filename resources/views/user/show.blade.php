69b00452db37970d2823c30f221a7c2d6d7352c2
gigi603 <gilbert.trinidad1@gmail.com>
2020-12-11 03:55:58 +0100

    isset

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -182,33 +182,35 @@
                             @endforeach
                         
                             @if (Auth::check())
-                                @if(count($client_reserved) > 0 && count($commentUser) == 0)
-                                    <div class="panel panel-default" style="margin: 0; border-radius: 0;">
-                                        <div class="panel-body">
-                                            <form action="{{ url('/comments') }}" method="POST" style="display: flex;">
-                                                {{ csrf_field() }}
-                                                <input type="hidden" name="house_id" value="{{ $house->id }}">
-                                                <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
-                                                <input type="hidden" name="admin_id" value="0">
-                                                <input type="hidden" name="reservation_id" value="0">  
-                                                <input type="hidden" name="parent_id" value=""> 
-                                                <div class="col-lg-7 col-md-7 col-sm-12 col-xs-12">
-                                                    <label for="input_comment" class="label-home">Saisir votre avis</label>
-                                                    <input type="text" name="comment" placeholder="Saisir un commentaire" class="form-control" id="input_comment" style="border-radius: 0;">
-                                                </div>
-                                                <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 rating">
-                                                    <input type="radio" id="star5" name="note" value="5" /><label for="star5" title="Meh">5 stars</label>
-                                                    <input type="radio" id="star4" name="note" value="4" /><label for="star4" title="Kinda bad">4 stars</label>
-                                                    <input type="radio" id="star3" name="note" value="3" /><label for="star3" title="Kinda bad">3 stars</label>
-                                                    <input type="radio" id="star2" name="note" value="2" /><label for="star2" title="Sucks big tim">2 stars</label>
-                                                    <input type="radio" id="star1" name="note" value="1" /><label for="star1" title="Sucks big time">1 star</label>
-                                                </div>
-                                                <div class="col-lg-2 col-md-2 col-sm-12 col-xs-12">
-                                                    <input type="submit" value="Envoyer" class="btn btn_reserve" style="border-radius: 0;">
-                                                </div>
-                                            </form>
+                                @if(isset($client_reserved))
+                                    @if(count($client_reserved) > 0 && count($commentUser) == 0)
+                                        <div class="panel panel-default" style="margin: 0; border-radius: 0;">
+                                            <div class="panel-body">
+                                                <form action="{{ url('/comments') }}" method="POST" style="display: flex;">
+                                                    {{ csrf_field() }}
+                                                    <input type="hidden" name="house_id" value="{{ $house->id }}">
+                                                    <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
+                                                    <input type="hidden" name="admin_id" value="0">
+                                                    <input type="hidden" name="reservation_id" value="0">  
+                                                    <input type="hidden" name="parent_id" value=""> 
+                                                    <div class="col-lg-7 col-md-7 col-sm-12 col-xs-12">
+                                                        <label for="input_comment" class="label-home">Saisir votre avis</label>
+                                                        <input type="text" name="comment" placeholder="Saisir un commentaire" class="form-control" id="input_comment" style="border-radius: 0;">
+                                                    </div>
+                                                    <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 rating">
+                                                        <input type="radio" id="star5" name="note" value="5" /><label for="star5" title="Meh">5 stars</label>
+                                                        <input type="radio" id="star4" name="note" value="4" /><label for="star4" title="Kinda bad">4 stars</label>
+                                                        <input type="radio" id="star3" name="note" value="3" /><label for="star3" title="Kinda bad">3 stars</label>
+                                                        <input type="radio" id="star2" name="note" value="2" /><label for="star2" title="Sucks big tim">2 stars</label>
+                                                        <input type="radio" id="star1" name="note" value="1" /><label for="star1" title="Sucks big time">1 star</label>
+                                                    </div>
+                                                    <div class="col-lg-2 col-md-2 col-sm-12 col-xs-12">
+                                                        <input type="submit" value="Envoyer" class="btn btn_reserve" style="border-radius: 0;">
+                                                    </div>
+                                                </form>
+                                            </div>
                                         </div>
-                                    </div>
+                                    @endif
                                 @endif
                             @endif
                         </div>
