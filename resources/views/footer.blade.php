21b16a4780ee9cb953f89573e1cb586a4a9e3b03
gigi603 <gilbert.trinidad1@gmail.com>
2021-01-08 00:56:10 +0100

    navbar footer blade and remove useful css, put css in blade to home page and layout

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,28 @@
+<footer class="footer" role="footer">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-offset-2 col-md-3">
+                <ul>
+                    <li><a href="{{ route('mentions_legales') }}">Mentions légales</a></li>
+                    <li><a href="{{ route('politique_de_confidentialite') }}">Politique de confidentialité</a></li>
+                    <li><a href="{{ route('cgu') }}">Conditions générales d'utilisation</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3">
+                <ul>
+                    <li><a href="{{ route('apropos') }}">A propos</a></li>
+                    <li><a href="{{ route('faq') }}">FAQ</a></li>
+                    <li><a href="{{ route('cgv') }}">Conditions générales de vente</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3">
+                <ul id="reseaux">
+                    <div><li><a href="https://www.facebook.com" target="_blank"><i class="fab fa-facebook-square fa-2x" aria-hidden="true"></i><span class="sr-only">Facebook</span></i></a></li></div>
+                    <div><li><a href="https://twitter.com" target="_blank"><i class="fab fa-twitter-square fa-2x" aria-hidden="true"><span class="sr-only">Twitter</span></i></a></li></div>
+                    <div><li><a href="https://www.instagram.com" target="_blank"><i class="fab fa-instagram fa-2x" aria-hidden="true"><span class="sr-only">Instagram</span></i></a></li></div>
+                    <div><li><a href="https://www.youtube.com" target="_blank"><i class="fab fa-youtube-square fa-2x" aria-hidden="true"><span class="sr-only">Youtube</span></i></a></li></div>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
